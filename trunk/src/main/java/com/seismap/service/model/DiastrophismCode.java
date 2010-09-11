2ea14ef43b2a64f3041a43420e9a9e4c8695d32a
emiliorecio <emiliorecio@516d055a-d912-3381-ece4-782178a81d90>
2010-09-11 18:06:26 +0000

    parser type2

---
+++ trunk/src/main/java/com/seismap/service/model/DiastrophismCode.java
@@ -0,0 +1,11 @@
+package src.main.java.com.seismap.service.model;
+
+public enum DiastrophismCode {
+	
+	SURFACE,
+	
+	UPLIFT,
+	
+	FAULTING
+
+}
---
+++ trunk/src/main/java/com/seismap/service/model/TsunamiCode.java
@@ -0,0 +1,9 @@
+package src.main.java.com.seismap.service.model;
+
+public enum TsunamiCode {
+	
+	TSUNAMI_GENERATED,
+	
+	POSSIBLE_TSUNAMI
+
+}
