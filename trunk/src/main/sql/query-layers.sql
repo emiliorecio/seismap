bed5a5d089b47b910930e1cfc3777c70ee710b4a
enrique.dacostacambio@gmail.com <enrique.dacostacambio@gmail.com@516d055a-d912-3381-ece4-782178a81d90>
2012-12-17 01:50:15 +0000

    query layer info

--- trunk/src/main/sql/query-layers.sql
+++ trunk/src/main/sql/query-layers.sql
@@ -1,3 +1,16 @@
+/*
+name: seismap:eventandaveragemagnitudes
+sql view
+parameter:
+ - Name: magnitudeType
+ - Default value: ML
+ - Validation regular expression: ^[\w]+$
+Attributes:
+- Identifier: id
+- Geometry: SRID 900913
+
+sql view:
+*/
 SELECT
     id as fid,
     location as pointlocation,
@@ -7,6 +20,19 @@ SELECT
     date_part('epoch', now() - date) as age
   FROM eventandaveragemagnitudes
 
+/*
+name: seismap:eventandaveragemagnitudes_depthlocation
+sql view
+parameter:
+ - Name: magnitudeType
+ - Default value: ML
+ - Validation regular expression: ^[\w]+$
+Attributes:
+- Identifier: id
+- Geometry: SRID 900913
+
+sql view:
+*/
 SELECT
     id as fid,
     depthlocation as pointlocation,
